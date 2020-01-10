f996968bd417b622cdf7889c97f31f847034be70
miyamaruyuuki <ajptmjg@gmail.com>
2020-01-10 17:11:28 +0900

    1711

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 Route::get('/', 'GameController@index');
 Route::get('/search', 'GameController@search');
 Route::get('play/', 'GameController@play');
-Route::get('play/{gid}/mid/{mid}/', 'GameController@play');
+Route::get('play/{gid}/{mid}/squeeze', 'GameController@play');
 Route::get('/playcount', 'GameController@playcount');
 
 Route::post('/playaddkakunin', 'GameController@playaddkakunin');
